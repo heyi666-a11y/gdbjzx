2d4571acaf6dcd5e2364631a8bd5f610d1fa6906
heyi666-a11y <3771269361@qq.com>
2026-04-06 16:17:16 +0800

    删除旧文件夹 校园网

--- 校园网/database/check-messages-table.sql
+++
@@ -1,5 +0,0 @@
--- 查看 messages 表的结构
-SELECT column_name, data_type
-FROM information_schema.columns
-WHERE table_name = 'messages'
-ORDER BY ordinal_position;
--- 校园网/database/check-test-users.sql
+++
@@ -1,9 +0,0 @@
--- 查看 profiles 表中叫"测试"的用户
-SELECT id, name, user_type, is_vip, student_id
-FROM profiles
-WHERE name = '测试';
-
--- 查看所有用户（用于对比）
-SELECT id, name, user_type, is_vip, student_id
-FROM profiles
-ORDER BY name, user_type;
